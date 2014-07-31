d3228797914fe869c7ae2111e9bae577c38d1e9c
josephktcheung <josephktcheung@gmail.com>
2014-07-31 12:05:41 +0800

    Add background image files

---
+++ pirate adventure 2/PAFactory.m
@@ -0,0 +1,13 @@
+//
+//  PAFactory.m
+//  pirate adventure 2
+//
+//  Created by Joseph Cheung on 31/7/14.
+//  Copyright (c) 2014 Reque.st. All rights reserved.
+//
+
+#import "PAFactory.h"
+
+@implementation PAFactory
+
+@end
