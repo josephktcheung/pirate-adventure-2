4a849e1ba68682614611ed3a0ac8db1c6bbb9232
josephktcheung <josephktcheung@gmail.com>
2014-07-31 14:10:35 +0800

    Create character class

---
+++ pirate adventure 2/PACharacter.m
@@ -0,0 +1,13 @@
+//
+//  PACharacter.m
+//  pirate adventure 2
+//
+//  Created by Joseph Cheung on 31/7/14.
+//  Copyright (c) 2014 Reque.st. All rights reserved.
+//
+
+#import "PACharacter.h"
+
+@implementation PACharacter
+
+@end
