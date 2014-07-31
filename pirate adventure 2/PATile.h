4b677bd86479db57a72794ad2b582ed0f98d5f7f
josephktcheung <josephktcheung@gmail.com>
2014-07-31 11:50:46 +0800

    Add tile class

---
+++ pirate adventure 2/PATile.h
@@ -0,0 +1,18 @@
+//
+//  PATile.h
+//  pirate adventure 2
+//
+//  Created by Joseph Cheung on 31/7/14.
+//  Copyright (c) 2014 Reque.st. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface PATile : NSObject
+
+@property (strong, nonatomic) NSString *story;
+@property (strong, nonatomic) NSString *action;
+@property (strong, nonatomic) UIImage *backgroundImage;
+
+@end
---
+++ pirate adventure 2/PATile.m
@@ -0,0 +1,13 @@
+//
+//  PATile.m
+//  pirate adventure 2
+//
+//  Created by Joseph Cheung on 31/7/14.
+//  Copyright (c) 2014 Reque.st. All rights reserved.
+//
+
+#import "PATile.h"
+
+@implementation PATile
+
+@end
