6421e2e070f52ac68078d8af11ce46cd706fb8fd
josephktcheung <josephktcheung@gmail.com>
2014-07-31 14:17:28 +0800

    Add weapon, armor and health effect in tile header

--- pirate adventure 2/PATile.h
+++ pirate adventure 2/PATile.h
@@ -8,11 +8,16 @@
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
+#import "PAWeapon.h"
+#import "PAArmor.h"
 
 @interface PATile : NSObject
 
 @property (strong, nonatomic) NSString *story;
 @property (strong, nonatomic) NSString *action;
 @property (strong, nonatomic) UIImage *backgroundImage;
+@property (strong, nonatomic) PAWeapon *weapon;
+@property (strong, nonatomic) PAArmor *armor;
+@property (nonatomic) int healthEffect;
 
 @end
