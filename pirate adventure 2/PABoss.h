e03d6ea72104e0893c907a0ac4137154af1fa451
josephktcheung <josephktcheung@gmail.com>
2014-07-31 15:10:52 +0800

    Create boss class

---
+++ pirate adventure 2/PABoss.h
@@ -0,0 +1,15 @@
+//
+//  PABoss.h
+//  pirate adventure 2
+//
+//  Created by Joseph Cheung on 31/7/14.
+//  Copyright (c) 2014 Reque.st. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PABoss : NSObject
+
+@property int health;
+
+@end
